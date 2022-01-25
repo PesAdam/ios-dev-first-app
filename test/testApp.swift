74d5d0fba0663060bd7fe09f9e523ff8ed3266c6
PesAdam <pesadam15@gmail.com>
2022-01-25 20:22:58 +0100

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Bender on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
